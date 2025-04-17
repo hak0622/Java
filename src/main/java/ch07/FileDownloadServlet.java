d07fa0f04fe18652f5b902858190cf60f3b694c8
hak0622 <seounghak062@naver.com>
2025-04-17 16:32:25 +0900

    0417심화과제

---
+++ src/main/java/ch07/FileDownloadServlet.java
@@ -0,0 +1,7 @@
+package ch07;
+
+public class FileDownloadServlet extends HttpServlet{
+    public void service(){
+        System.out.println("로그인 합니다.");
+    }
+}
---
+++ src/main/java/ch07/LoginServlet.java
@@ -0,0 +1,7 @@
+package ch07;
+
+public class LoginServlet extends HttpServlet{
+    public void service(){
+        System.out.println("파일 다운로드  합니다.");
+    }
+}
