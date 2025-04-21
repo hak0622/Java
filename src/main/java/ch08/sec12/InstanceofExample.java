cd50af330c01cdb9a5c16120db1b69e430ba80a2
hak0622 <seounghak062@naver.com>
2025-04-21 16:06:18 +0900

    0421심화과제

---
+++ src/main/java/ch08/sec12/InstanceofExample.java
@@ -0,0 +1,23 @@
+package ch08.sec12;
+
+public class InstanceofExample {
+    public static void main(String[] args) {
+        Taxi taxi = new Taxi();
+        Bus bus = new Bus();
+
+        ride(taxi);
+//        System.out.println();
+        ride(bus);
+    }
+    public static void ride(Vehicle vehicle){
+        if(vehicle instanceof Taxi){
+            Taxi taxi = (Taxi)vehicle;
+            taxi.run();
+        }
+        if(vehicle instanceof Bus){
+            Bus bus = (Bus)vehicle;
+            bus.checkFare();
+            bus.run();
+        }
+    }
+}
