5982870c74bae04f1c64d018bca2585ab928d8b9
hak0622 <seounghak062@naver.com>
2025-04-22 14:35:16 +0900

    0422기본과제

---
+++ src/main/java/ch09/sec06/exam03/ButtonExample.java
@@ -0,0 +1,24 @@
+package ch09.sec06.exam03;
+
+public class ButtonExample {
+    public static void main(String[] args) {
+        Button btnOk = new Button();
+        class OkListener implements Button.ClickListener{
+            public void onClick(){
+                System.out.println("ok 버튼을 클릭하였습니다.");
+            }
+        }
+        btnOk.setClickListener(new OkListener());
+        btnOk.click();
+
+        Button btnCancel = new Button();
+
+        class CancelListener implements Button.ClickListener{
+            public void onClick(){
+                System.out.println("Cancel 버튼을 클릭");
+            }
+        }
+        btnCancel.setClickListener(new CancelListener());
+        btnCancel.click();
+    }
+}
